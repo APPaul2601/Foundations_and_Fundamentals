aa06b80c8340ce8a01dc17b88968be8071c7dd6b
Paul Popescu <153536502+APPaul2601@users.noreply.github.com>
2024-03-18 16:30:25 +0200

    Add files via upload

---
+++ Chap04/Guessing_game.py
@@ -0,0 +1,10 @@
+fave_fruit = "Orange"
+
+question = input("What's my favorite food? ")
+
+if question == fave_fruit:
+    print("Yep! So amazing")
+elif question != fave_fruit:
+    print("Yuck! That's not it!")
+else:
+    print("Thanks for playing!")
---
+++ Chap05/fav_city.py
@@ -0,0 +1,13 @@
+def fav_city(name):
+    print("My favorite city is", name)
+
+
+fav_city("Athens")
+fav_city("Bucharest")
+fav_city("London")
+
+
+def compare():
+    print(5, "is bigger", 6)
+
+compare()
